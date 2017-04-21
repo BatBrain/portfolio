d58f88077a482aec3d6b8b6977f3f5db225117e0
Sol <sol@Sols-MBP.ccs>
2017-04-20 20:41:22 -0400

    minimum viable portfolio website

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,70 @@
-jax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(function() {
+
+  // Set up defaults for JS-enabled browser
+  $('nav li:first-child').addClass('active');;
+  $('.more-less-content').hide();
+  $('.more-less-toggle').show();
+
+  // Event listeners
+  $(document).on('scroll', onScroll);
+  $('nav a').on('click', onNavClick);
+  $('.more-less-toggle').on('click', onMoreLessClick);
+
 });
+
+function onScroll(event) {
+  // Detemine current position
+  var scrollPos = $(document).scrollTop();
+
+  // For each link, remove active class
+  // Add active class for current section
+  $('nav a').each(function() {
+    var $currentLink = $(this);
+    var refElement = $($currentLink.attr('href'));
+
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('nav ul li').removeClass("active");
+      $currentLink.parent().addClass("active");
+    } else {
+      $currentLink.parent().removeClass("active");
+    }
+  });
+}
+
+function onNavClick(event) {
+  event.preventDefault();
+
+  $(document).off('scroll');
+
+  // Remove active class from all nav list items,
+  // then add it to selected list item
+  $('nav li').each(function() {
+    $(this).removeClass('active');
+  });
+  // Add active class to selected list item
+  $(this).parent().addClass('active');
+
+  // Determine selected section and navigate to it
+  var target = this.hash;
+  $target = $(target);
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top+2
+  }, 'slow', 'swing', function() {
+      $(document).on("scroll", onScroll);
+  });
+}
+
+function onMoreLessClick(event) {
+  event.preventDefault();
+
+  $(document).off('scroll');
+
+  // Perform slide-toggle on preceding content area
+  $(this).prev('.more-less-content').slideToggle('slow', function() {
+    $(document).on('scroll', onScroll);
+  });
+
+  // Change text for more/less toggle
+  if ($(this).text() == '+ more') { $(this).text('- less'); }
+  else { $(this).text('+ more'); }
+}
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
+// const partials    = require('express-partials');
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
-
 const morgan      = require('morgan');
 
 // Seperated Routes for each Resource
@@ -19,8 +19,10 @@ const usersRoutes = require("./routes/users");
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
+// app.set('views', __dirname + '/views');
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(partials());
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
